cbf659af1f8a01b9cba11f3c8e27c635278528a8
TaVitka <tavitka17@gmail.com>
2024-04-01 22:56:28 +0300

    added products section for home page

---
+++ src/js/components/_slider.js
@@ -0,0 +1,26 @@
+import Swiper, { Navigation, Pagination, EffectFade, Scrollbar } from 'swiper';
+
+let options = {
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  spaceBetween: 24,
+  autoHeight: true,
+  speed: 700,
+  autoplay: {
+    delay: 200,
+  },
+  modules: [Navigation, Scrollbar, Pagination, EffectFade],
+  draggable: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    clickable: true,
+    el: '.swiper-pagination',
+  },
+}
+
+const swiperProducts = new Swiper('.products__slider', options);
